56db93a279784dbb6309b201479749539112ae87
SirPripyat <08.leo.rossi@gmail.com>
2024-01-13 01:17:37 -0300

    Implementando barra de pesquisa

--- src/components/home/Pokemons/Pokemons.tsx
+++ src/components/home/Pokemons/Pokemons.tsx
@@ -2,16 +2,20 @@ import { Pokemon } from "@/types/pokemon";
 import PokemonCard from "../PokemonCard/PokemonCard";
 import useFetchPokemons from "./useFetchPokemons";
 import LoaderScreen from "@/components/lib/LoaderScreen/LoaderScreen";
+import { usePokemonDataStore } from "@/store/pokemonDataStore";
 
 export default function Pokemons() {
-  const { pokemons, isLoading } = useFetchPokemons();
+  const { isLoading } = useFetchPokemons();
+
+  const { pokemonData } = usePokemonDataStore();
+  const pokemons = pokemonData && pokemonData.pokemons;
 
   return (
     <>
       {isLoading && <LoaderScreen />}
       {!isLoading && (
         <div className="w-full grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3 xl:grid-cols-4">
-          {pokemons.map((pokemon: Pokemon) => (
+          {pokemons?.map((pokemon: Pokemon) => (
             <PokemonCard key={pokemon.index} {...pokemon} />
           ))}
         </div>
