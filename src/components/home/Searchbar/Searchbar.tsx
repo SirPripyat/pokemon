84ee46da3b853291d945ba2c74736bf7a26eec01
SirPripyat <08.leo.rossi@gmail.com>
2024-01-14 22:11:47 -0300

    Corrigindo texto e remodelando CSS da serachbar

--- src/components/home/Searchbar/Searchbar.tsx
+++ src/components/home/Searchbar/Searchbar.tsx
@@ -5,13 +5,13 @@ export default function Searchbar() {
   const { search, handleSearchChange } = useSearchbar();
 
   return (
-    <div className="relative w-full sm:w-1/2 lg:w-1/3">
+    <div className="relative w-full sm:w-fit">
       <input
-        className="w-full py-2 px-4 pl-12 rounded-full bg-transparent border-2 border-red-600 focus:outline-none focus:border-red-800 transition duration-300 ease-in-out "
+        className="w-full py-2 px-4 pl-12 rounded-full bg-transparent border border-red-600 focus:outline-none focus:border-red-800 transition duration-300 ease-in-out "
         type="text"
         value={search}
         onChange={handleSearchChange}
-        placeholder="Search for a pokemon"
+        placeholder="Search for a pokémon"
       />
       <Search className="left-4 absolute top-1/2 transform -translate-y-1/2 text-red-600  transition duration-300 ease-in-out" />
     </div>
