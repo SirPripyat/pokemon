9838810a3d35095d6d5c511448bc67dadcb27e30
SirPripyat <08.leo.rossi@gmail.com>
2024-01-15 22:26:00 -0300

    ajustes finos para a funcionalidade de filtro rodar 100%

--- src/store/pokemonDataStore/index.tsx
+++ src/store/pokemonDataStore/index.tsx
@@ -1,4 +1,3 @@
-import { PokemonsTypes } from "@/types/pokemonsTypes";
 import { ReadAllPokemonsResponse } from "@/types/readAllPokemonsResponse";
 import { create } from "zustand";
 
@@ -7,31 +6,11 @@ type PokemonDataStore = {
   setPokemonData: (pokemonData: ReadAllPokemonsResponse["data"]) => void;
 };
 
-const pokemonDataDefaultValue = {
-  currentPage: 1,
-  numberOfPokemons: 1,
-  pokemons: [
-    {
-      index: 1,
-      name: "Bulbasaur",
-      pokedexNumber: "001",
-      image: "",
-      pokemonTypes: ["grass", "poison"] as PokemonsTypes[],
-    },
-  ],
-  totalPages: 1,
-};
-
 export const usePokemonDataStore = create<PokemonDataStore>((set) => ({
   pokemonData: {} as ReadAllPokemonsResponse["data"],
   setPokemonData: (pokemonData) => {
-    const lastPage = pokemonData.totalPages - 1;
-
     set({
-      pokemonData: {
-        ...pokemonData,
-        totalPages: lastPage,
-      },
+      pokemonData,
     });
   },
 }));
