8c543bc967e503c194a3824c2106d1143cf35653
SirPripyat <08.leo.rossi@gmail.com>
2024-01-21 11:39:24 -0300

    Removendo hook que altera a rota do usuário a cada refresh de página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import { AllPokemonsSection } from "@/components/home/AllPokemonsSection/AllPokemonsSection";
-import usePushFirstPageOnRoute from "@/globalHooks/usePushFirstPageOnRoute";
 import useRemovePokemonsOnFilter from "@/globalHooks/useRemovePokemonsOnFilter";
 
 export default function Home() {
-  usePushFirstPageOnRoute();
   useRemovePokemonsOnFilter();
 
   return (
