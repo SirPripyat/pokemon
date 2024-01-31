211b3b080872cbf13cb23509a7d53ae9823a6bd7
SirPripyat <08.leo.rossi@gmail.com>
2024-01-30 21:35:05 -0300

    criando função para add  array de strings no route query

--- src/globalHooks/useCreateQueryString.tsx
+++ src/globalHooks/useCreateQueryString.tsx
@@ -16,7 +16,29 @@ export default function useCreateQueryString() {
       [searchParams]
     );
 
+  const search = searchParams.get("search");
+  const page = searchParams.get("page");
+
+  const createQueryStringWithMultipleValues: (
+    name: string,
+    values: string[]
+  ) => string = useCallback(
+    (name: string, values: string[]) => {
+      const baseUrl = `search=${search}&page=${page}`;
+
+      const params = new URLSearchParams(baseUrl);
+
+      values.forEach((value) => {
+        params.append(name, value);
+      });
+
+      return params.toString();
+    },
+    [page, search]
+  );
+
   return {
     createQueryString,
+    createQueryStringWithMultipleValues,
   };
 }
