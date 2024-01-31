8047fb7ee1f3780409420ef9ee7af31a3d24199b
SirPripyat <08.leo.rossi@gmail.com>
2024-01-30 21:03:01 -0300

    add button para pesquisar poke

--- src/components/home/Searchbar/Searchbar.tsx
+++ src/components/home/Searchbar/Searchbar.tsx
@@ -2,18 +2,24 @@ import { Search } from "lucide-react";
 import useSearchbar from "./useSearchbar";
 
 export default function Searchbar() {
-  const { search, handleSearchChange } = useSearchbar();
+  const { search, handleSearchChange, pushSearch } = useSearchbar();
 
   return (
-    <div className="relative w-full sm:w-fit">
+    <form className="relative w-full sm:w-fit rounded-full overflow-hidden">
       <input
-        className="w-full py-2 px-4 pl-12 rounded-full bg-transparent border border-red-600 focus:outline-none focus:border-red-800 transition duration-300 ease-in-out "
+        className="w-full py-2 px-4 pr-12 rounded-full bg-transparent border border-red-600 focus:outline-none focus:border-red-800 transition duration-300 ease-in-out "
         type="text"
         value={search}
         onChange={handleSearchChange}
         placeholder="Search for a pokémon"
       />
-      <Search className="left-4 absolute top-1/2 transform -translate-y-1/2 text-red-600  transition duration-300 ease-in-out" />
-    </div>
+      <button
+        type="submit"
+        onClick={(event) => pushSearch(event)}
+        className="bg-red-600 right-0 absolute top-1/2 transform -translate-y-1/2 h-full w-14 flex justify-center items-center"
+      >
+        <Search className=" text-zinc-50 transition duration-300 ease-in-out" />
+      </button>
+    </form>
   );
 }
